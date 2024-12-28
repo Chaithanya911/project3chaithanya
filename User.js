149050f195618aae0fac74c84ff90ad1df3e21d7
Chaithanya911 <chaithanya0911@gmail.com>
2024-12-28 11:44:01 +0530

    Delete User.js

--- User.js
+++
@@ -1,8 +0,0 @@
-const mongooes = require('mongoose');
-
-const userSchema = new mongooes.Schema({
-    email: {type: String, required: true, unique: true},
-    password: { type: String, required: true }
-});
-
-module.exports = mongooes.model('User',userSchema);
